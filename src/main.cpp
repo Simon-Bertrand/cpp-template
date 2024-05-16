f281049a166f8e295a9401fb1a369fb195cb42f1
Simon Bertrand <simonbertrand.contact@gmail.com>
2024-05-16 19:42:34 +0200

    init torch

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,11 @@
 #include "core/MyObject.hpp"
 #include <stdio.h>
+#include <torch/torch.h>
 
 int main() {
   printf("Hello, World!\n");
-
+  torch::Tensor tensor = torch::rand({10, 10});
+  std::cout << tensor << std::endl;
   MyObject myObject2(0);
   myObject2.printAttribute();
 }
